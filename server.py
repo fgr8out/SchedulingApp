b09e44830bc6aa37105f860f81bbd4ddb1d7adeb
fgr8out <fgr8out@icloud.com>
2015-11-16 20:58:24 -0800

    webform complete; posting to db is active and dashboard has been added

--- server.py
+++ server.py
@@ -1,14 +1,9 @@
-"""NCCC Training Schedule"""
-
-from jinja2 import StrictUndefined
-
-from flask import Flask, render_template, redirect, request, flash, session 
-
+from flask import Flask, flash, json, redirect, render_template, request, send_file, session
 from flask_debugtoolbar import DebugToolbarExtension
-
+from datetime import datetime, timedelta
+from jinja2 import StrictUndefined
 from model import connect_to_db, db
-
-from model import Unit, Team, Training, TrainingAssignment, Staff, Building, Room
+from model import *
 
 app = Flask(__name__)
 
@@ -21,16 +16,15 @@ app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
 def splash_login():
-    """This is where the user will login"""
+    """Login Page"""
 
 
     return render_template("login.html")
 
 
-
 @app.route('/schedule')
 def choose_schedule():
-    """Homepage."""
+    """Main Scheduling Page"""
 
     teams = Team.query.all()
     units = Unit.query.all()
@@ -38,19 +32,88 @@ def choose_schedule():
     trainings = Training.query.all()
 
 
-
-
     return render_template("schedule.html", teams=teams, trainings=trainings, 
                                             units=units, staff=staff)
 
+@app.route('/submit_schedule', methods=['POST'])
+def submit_schedule():
+    "Submit/Post webform to db"
+    
 
-# needs a post method @ some point
+    team = request.form['team_name']
+    print team
+    training = request.form['training_name']
+    print training
+    start_date = request.form['start_date']
+    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+    print type(start_date)
+    end_date = request.form['end_date']
+    end_date = datetime.strptime(end_date, '%Y-%m-%d')
+    print type(end_date)
+    start_time = request.form['start_time']
+    start_time = datetime.strptime(start_time, '%H:%M')
+    print type(start_time)
+    end_time = request.form['end_time']
+    end_time = datetime.strptime(end_time, '%H:%M')
+    print type(end_time)
+
+        
+
+        
+        
+    #Creates new user in DB
+    new_training = TrainingAssignment(
+                                 team_id=team,
+                                 training_id=training,
+                                 start_date=start_date,
+                                 end_date=end_date,
+                                 start_time=start_time,
+                                 end_time=end_time)
+
+    db.session.add(new_training)
+    db.session.commit()
+
+
+    flash("Schedule Request has been created for {}".format(team))
+
+    return redirect("/schedule.html")
+
+
+
 @app.route('/dashboard')
 def process_request():
     """Show result of schedule choices on dashboard."""
 
 
     return render_template("dashboard.html")
+
+
+
+# @app.route("/login", methods=['GET'])
+# def login_page():
+#     """Displays login page"""
+
+#     return render_template("login.html")
+
+
+
+# @app.route("/login", methods=['POST'])
+# def login():
+#     """Logs in the user, or denies access"""
+
+#     email = request.form.get("email")
+#     password = request.form.get("password")
+    
+#     user = User.authenticate(email, password)
+
+#     if user:
+#         session["user_id"] = user.user_id
+#         session["fname"] = user.fname
+#         return redirect("/trips")
+
+#     else:
+#         flash("Your information could not be found in the system. Try again or sign up!")
+#         return redirect("/login")
 
 
 # @app.route('/register', methods=['POST'])
@@ -184,7 +247,64 @@ def process_request():
 
 #     return redirect("/movies/%s" % movie_id)
 
+# @app.route("/pdf", methods=["POST"])
+# def generate_pdf():
+#     """Generates a PDF of the itinerary"""
 
+#     trip_id = int(request.form['tripId'])
+#     trip = Trip.query.get(trip_id)
+#     filename = "itinerary%d.pdf" % (trip_id)
+#     trip.generate_itinerary(filename)
+
+#     response_dict = {'filename': filename}
+#     response = json.dumps(response_dict)
+
+#     return response
+
+# @app.route("/send_text", methods=["POST", "GET"])
+# def send_reminders():
+#     """Sends reminders to trip viewers"""
+
+#     trip_id = int(request.form['tripId'])
+#     trip = Trip.query.get(trip_id)
+
+#     trip.send_SMS(tw_sid, tw_token)
+
+#     return "Success"
+
+
+# @app.route("/itinerary<int:trip_id>", methods=['GET', 'POST'])
+# def show_pdf(trip_id):
+#     """Displays the PDF itinerary"""
+
+#     filename = 'itinerary%r.pdf' % (trip_id)
+#     itinerary = open(filename, 'rb')
+
+#     return send_file(itinerary)
+
+# #############################################################
+# # Jinja Filter
+
+# @app.template_filter('datetime')
+# def _format_datetime(dt, format=None, trip_end=False):
+#     """Formats a datetime object for display """
+
+#     if trip_end:
+#         dt = dt - timedelta(1)
+
+#     if format == 'time':
+#         dt = datetime.strftime(dt, '%-I:%M %p')
+#     elif format == 'date':
+#         dt = datetime.strftime(dt, '%b %-d, %Y')
+#     elif format == 'date-short':
+#         dt = datetime.strftime(dt, '%b %-d')
+#     elif format == 'datetime pretty':
+#         dt = datetime.strftime(dt, '%-I:%M %p, %b %d, %Y')
+#     else:
+#         dt = datetime.strftime(dt, '%Y-%m-%dT%H:%M:%SZ')
+
+    # return dt
+###############################################################################
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
